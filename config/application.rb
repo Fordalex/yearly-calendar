69bac4e998f8fcd87c40d43ffd32bdb52119be90
Fordalex <alex96ford19@gmail.com>
2021-12-12 15:48:26 +0000

    loads lib folder

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Bbonds
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.paths.add "lib", eager_load: true
+    config.eager_load_paths += Dir["#{config.root}/lib"]
   end
 end
