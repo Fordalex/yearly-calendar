258c192bd539e4336eb10bfd15c6cb5ce0c3062a
Fordalex <alex96ford19@gmail.com>
2021-12-31 23:37:22 +0000

    refactor

--- lib/calendar.rb
+++ lib/calendar.rb
@@ -3,12 +3,16 @@ class Calendar
   include ActionView::Context
 
   def available_dates
-    Availability.all.pluck(:date)
+    @available_dates ||= Availability.all.pluck(:date)
   end
 
   def show_calendar
     date = Date.today
-    create_calendar_month(date)
+    calendars = ActiveSupport::SafeBuffer.new("")
+    3.times do |i|
+      calendars += create_calendar_month(date + i.month)
+    end
+    calendars
   end
 
   def create_calendar_month(date)
@@ -43,23 +47,22 @@ class Calendar
 
     # Create offset or start days
     if offset > 1
-      days = tag.div
-      (offset - 2).times {|i| days += tag.div}
-      days += tag.div "01"
-    else
-      days = tag.div "01"
+      offset_divs = tag.div
+      (offset - 2).times {|i| offset_divs += tag.div}
     end
 
     # Create all the numbers for that month
-    (2..days_in_month).each do |i|
-      num = i.to_s
-      num = num.length > 1 ? num : "0#{num}"
+    days = ActiveSupport::SafeBuffer.new("")
+    (1..days_in_month).each do |i|
+      num = i.to_s.length > 1 ? i : "0#{i}"
       date = Date.new(year, month, i)
       days += tag.div num, id: date, class: date_available(date)
     end
 
+    calendar_cells = days
+    calendar_cells.prepend(offset_divs) if offset > 1
     tag.div class: "calendar-numberContainer" do
-      days
+      calendar_cells
     end
   end
 
